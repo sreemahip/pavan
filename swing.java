8d6f862ccf747bdb5a75e28dbf671dee2e54e4c7
sreemahip <srimahi712@gmail.com>
2024-04-10 17:05:15 +0530

    this is second programm

---
+++ swing.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class swing{
+	public static void main(String[] args){
+		int a=23;
+		int b=24;
+		System.out.println("the two numbers addition are");
+		c=a+b;
+		System.out.println("the addition is:",c);
+	}
+}
